2b7c76f308564b0a8aa983e1d86c7e3eb1097a92
Biokes <106444765+Biokes@users.noreply.github.com>
2025-02-02 22:45:07 +0100

    update

--- app/(tabs)/contactScreen.tsx
+++ app/(tabs)/contactScreen.tsx
@@ -1,10 +1,12 @@
-import {StyleSheet, View} from 'react-native';
+import {SafeAreaView, StyleSheet, View} from 'react-native';
 import Contact from "@/components/contact/contact";
 import {Stack} from "expo-router";
 export default function ContactScreen() {
   return (
       <Stack.Screen options={{headerShown:false}}>
-        <Contact/>
+        <SafeAreaView>
+          <Contact/>
+        </SafeAreaView>
       </Stack.Screen>
   );
 }
